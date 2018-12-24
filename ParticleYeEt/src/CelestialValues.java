6be00ff983a7e987815a784b5d0fb4214694de2f
marcelnerd <31940874+marcelnerd@users.noreply.github.com>
2018-12-23 20:37:28 -0600

    test comment

--- ParticleYeEt/src/CelestialValues.java
+++ ParticleYeEt/src/CelestialValues.java
@@ -4,4 +4,5 @@ public final class CelestialValues {
 	public static final double SUN_MASS = 1;
 	public static final double MOON_MASS = (3.69396868 * Math.pow(10, -8));
 	public static final double MOON_DISTANCE = 0.00256955529;
+	//yeet
 }
